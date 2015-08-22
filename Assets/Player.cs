d6f804221504c2bdfbb38ca8ea350dd410390dde
Ed Paradis <legomaniac@gmail.com>
2015-08-21 18:55:25 -0700

    player falls downwards by default

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	bool _isFalling = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if( _isFalling)
+		{
+			transform.Translate(0, -8 * Time.deltaTime, 0);
+		}
+	}
+}
