826644ad344ba57b44f90123f98b4de79c30dab4
Ed Paradis <legomaniac@gmail.com>
2015-08-21 20:52:22 -0700

    touching a platform prevents falling

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,17 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
     bool _isFalling = true;
     public GameObject Ui;
+    List<Bounds> _platformBounds;
+    Bounds _bounds;
 
-    void Start(){}
+    void Start()
+    {
+        var platforms = GameObject.FindGameObjectsWithTag("platform");
+        _platformBounds = new List<Bounds>();
+        for( var i=0; i<platforms.Length; i+=1)
+        {
+            _platformBounds.Add(platforms[i].GetComponent<Renderer>().bounds);
+        }
+    }
 
     void Update()
     {
+        _bounds = GetComponent<Renderer>().bounds;
+
+        if( IsTouchingPlatform())
+            _isFalling = false;
+
         if( _isFalling)
-            transform.Translate(0, -8 * Time.deltaTime, 0);
+            transform.Translate(0, -0.16f , 0);
 
         if( HasReachedBottomOfScreen() )
             KillPlayer();
@@ -26,5 +42,16 @@ public class Player : MonoBehaviour {
     {
         Ui.SendMessage("ShowGameOver");
         _isFalling = false;
+    }
+
+    bool IsTouchingPlatform()
+    {
+        foreach( var platformBound in _platformBounds)
+        {
+            if( platformBound.Intersects( _bounds))
+                return true;
+        }
+
+        return false;
     }
 }
