07f242215c5cb13640cdc4e6525b8aa01cdfa724
Ed Paradis <legomaniac@gmail.com>
2015-08-22 13:13:37 -0700

    new jumping method; basic platforming complete

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,7 +9,6 @@ public class Player : MonoBehaviour {
     List<Bounds> _platformBounds;
     Bounds _bounds;
     int _jumpingCounter = 0;
-    float _allowableTravel;
 
     void Start()
     {
@@ -34,48 +33,41 @@ public class Player : MonoBehaviour {
         horizDistance = FindMaxHorizontalMovement( horizDistance);
         transform.Translate(horizDistance, 0, 0);
 
-        float vertDistance = 0;
+        /*float vertDistance = 0;
         if( Input.GetAxis("Vertical") > 0)
             vertDistance = 0.08f;
         if( Input.GetAxis("Vertical") < 0)
             vertDistance = -0.08f;
 
         vertDistance = FindMaxVerticalMovement( vertDistance);
-        transform.Translate(0, vertDistance, 0);
+        transform.Translate(0, vertDistance, 0);*/
 
-        /*if( IsTouchingPlatform() && Input.GetButtonDown("Jump"))
+        if( IsTouchingTopOfPlatform() && Input.GetButtonDown("Jump"))
             _jumpingCounter = 10;
 
-        if( IsTouchingPlatform())
+        if( IsTouchingTopOfPlatform())
             _isFalling = false;
 
-        float distance = 0;
+        float vertDistance = 0;
         if( _isFalling)
         {
-            if( WouldPassThroughTopOfPlatform(-0.16f))
-                distance = _allowableTravel; 
-            else
-                distance = -0.16f;
-            transform.Translate(0, distance, 0);
+            vertDistance = -0.16f;
         }
         else
         {
             if( _jumpingCounter > 0)
             {
                 _jumpingCounter -= 1;
-                if( WouldPassThroughBottomOfPlatform(0.32f))
-                {
-                    _jumpingCounter = 0;
-                    distance = _allowableTravel;
-                }
-                else
-                    distance = 0.32f;
-                transform.Translate(0, distance, 0);
+                vertDistance = 0.32f;
             }
+
             if( _jumpingCounter == 0)
                 _isFalling = true;
-        }*/
+        }
 
+        vertDistance = FindMaxVerticalMovement( vertDistance);
+        transform.Translate(0, vertDistance, 0);
+        
         if( HasReachedBottomOfScreen() )
             KillPlayer();
     }
@@ -91,150 +83,13 @@ public class Player : MonoBehaviour {
         _isFalling = false;
     }
 
-    bool IsTouchingPlatform()
+    bool IsTouchingTopOfPlatform()
     {
         foreach( var platformBound in _platformBounds)
         {
-            if( platformBound.Intersects( _bounds))
+            if( platformBound.Intersects( _bounds)
+                && Mathf.Abs(platformBound.max.y - _bounds.min.y) < 0.001f )
                 return true;
-        }
-
-        return false;
-    }
-
-    bool WouldPassThroughTopOfPlatform( float distance)
-    {
-        var newCharBound = new Bounds(_bounds.center + new Vector3( 0, distance, 0), _bounds.size);
-        foreach( var platformBound in _platformBounds)
-        {
-            if( platformBound.Intersects( newCharBound))
-            {
-                Debug.Log("intersect top");
-                if( OverlapTop( newCharBound, platformBound))
-                {
-                    _allowableTravel = GetAllowableTravelDownwards( _bounds, platformBound);
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-
-    bool OverlapTop(Bounds character, Bounds platform)
-    {
-        if( character.min.y < platform.max.y )
-        {
-            Debug.Log("would overlap top");
-            return true;
-        }
-        return false;
-    }
-
-    float GetAllowableTravelDownwards( Bounds character, Bounds platform)
-    {
-        return platform.max.y - character.min.y;
-    }
-
-    float GetAllowableTravelRightwards( Bounds character, Bounds platform)
-    {
-        return platform.min.x - character.max.x;
-    }
-
-    float GetAllowableTravelLeftwards( Bounds character, Bounds platform)
-    {
-        return platform.max.x - character.min.x;
-    }
-
-    float GetAllowableTravelUpwards( Bounds character, Bounds platform)
-    {
-        return platform.min.y - character.max.y;
-    }
-
-    bool OverlapRight( Bounds character, Bounds platform)
-    {
-        if( character.max.x > platform.min.x )
-        {
-            Debug.Log("would overlap right");
-            return true;
-        }
-        return false;
-    }
-
-    bool WouldTouchRightSideOfPlatform( float distance)
-    {
-        var newCharBound = new Bounds(_bounds.center + new Vector3( distance, 0, 0), _bounds.size);
-        foreach( var platformBound in _platformBounds)
-        {
-            if( platformBound.Intersects( newCharBound))
-            {
-                Debug.Log("intersect right");
-                if( OverlapTop( newCharBound, platformBound)
-                    && OverlapRight( newCharBound, platformBound))
-                {
-                    _allowableTravel = GetAllowableTravelRightwards( _bounds, platformBound);
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-
-    bool OverlapLeft( Bounds character, Bounds platform)
-    {
-        if( character.min.x < platform.max.x )
-        {
-            Debug.Log("would overlap left");
-            return true;
-        }
-        return false;
-    }
-    
-    bool WouldTouchLeftSideOfPlatform( float distance)
-    {
-        var newCharBound = new Bounds(_bounds.center + new Vector3( distance, 0, 0), _bounds.size);
-        foreach( var platformBound in _platformBounds)
-        {
-            if( platformBound.Intersects( newCharBound))
-            {
-                Debug.Log("intersect left");
-                if( OverlapTop( newCharBound, platformBound)
-                    && OverlapLeft( newCharBound, platformBound))
-                {
-                    _allowableTravel = GetAllowableTravelLeftwards( _bounds, platformBound);
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-
-    bool OverlapBottom( Bounds character, Bounds platform)
-    {
-        if( character.max.y > platform.min.y )
-        {
-            Debug.Log("would overlap bottom");
-            return true;
-        }
-        return false;
-    }
-    
-    bool WouldPassThroughBottomOfPlatform( float distance)
-    {
-        var newCharBound = new Bounds(_bounds.center + new Vector3( 0, distance, 0), _bounds.size);
-        foreach( var platformBound in _platformBounds)
-        {
-            if( platformBound.Intersects( newCharBound))
-            {
-                Debug.Log("intersect bottom");
-                if( OverlapBottom( newCharBound, platformBound))
-                {
-                    _allowableTravel = GetAllowableTravelUpwards( _bounds, platformBound);
-                    return true;
-                }
-            }
         }
         
         return false;
