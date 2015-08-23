e97aac99f574d01a64d761172cdde87ae25e271c
Ed Paradis <legomaniac@gmail.com>
2015-08-23 10:40:34 -0700

    add title screen and make default starting scene

---
+++ Assets/scripts/TitleScreen.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnStartButtonClick()
+    {
+    }
+
+    public void OnLevelEditorButtonClick()
+    {
+        Application.LoadLevel("level_builder");
+    }
+
+    public void OnStartSavedLevelButtonClick()
+    {
+        Application.LoadLevel("saved_level");
+    }
+}
